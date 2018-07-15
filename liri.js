82531ba0e847e0e7ed30b9b80bff945c209a5c80
Calvin <Calvin.d.Bowman@gmail.com>
2018-07-14 23:56:30 -0400

    time to turn it in

--- liri.js
+++ liri.js
@@ -1,11 +1,11 @@
-require("dotenv").config();
-console.log(dontev);
+require("dotenv").config(
+);
 var keys = require("./keys");
-var Spotify = require('node-spotify-api');
+var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
 
-var request = require('request');
 var fs = require("fs");
+var divider = "\n------------------------------------------------------------\n\n";
 
 var task = process.argv[2];
 var thingSearched = process.argv[3];
@@ -18,36 +18,44 @@ if (task === "movie-this") {
 
 
 function spotifySearch() {
-    if (thingSearched == null) {
+    console.log()
+    if (thingSearched === "Undefined") {
         thingSearched = 'The Sign';
     }
     spotify.search({
         type: 'track',
         query: thingSearched 
-    }, function(error, data) {
+    }, 
+    function(error, data) {
         if (error) {
             console.log('Error occurred: ' + error);
             return;
-            }
+            } else  {
             console.log('--------------------');
             console.log('Artist(s): ' + data.tracks.items[0].artists[0].name);
             console.log('Song Title: ' + data.tracks.items[0].name);
             console.log('Preview Link: ' + data.tracks.items[0].preview_url);
             console.log('Album: ' + data.tracks.items[0].album.name);
-            console.log('--------------------');
-    });
-}
+            console.log('--------------------')};
+
+            fs.appendFile("log.txt", spotify + divider, function(error) {
+                if (error) throw error;
+            });
+        }
+    )};
+            
+
 
 function movieSearch () {
     console.log(thingSearched);
     if (thingSearched === "Undefined"){
-        var queryUrl = "http://www.omdbapi.com/?t=Mr+Nobody&y=&plot=short&apikey=trilogy";
+        var movieMe = "http://www.omdbapi.com/?t=Mr+Nobody&y=&plot=short&apikey=trilogy";
     } else {
-        var queryUrl = "http://www.omdbapi.com/?t=" + thingSearched + "&y=&plot=short&apikey=trilogy";
+        var movieMe = "http://www.omdbapi.com/?t=" + thingSearched + "&y=&plot=short&apikey=trilogy";
     }
 
     var request = require("request");
-    request(queryUrl, function(error, response, body){
+    request(movieMe, function(error, response, body){
         if (!error && response.statusCode === 200){
             console.log("-----------------------");
             console.log("Title: " + JSON.parse(body).Title);
@@ -58,6 +66,11 @@ function movieSearch () {
             console.log("Plot: " + JSON.parse(body).Plot);
             console.log("Actors: " + JSON.parse(body).Actors);
             console.log("-----------------------");
+
+            fs.appendFile("log.txt", request + divider, function(error) {
+                if (error) throw error;
+            });
+
             
         }
     })
